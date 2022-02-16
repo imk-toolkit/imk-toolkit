12fcae61ae94095f4137294a38868d6ac6a9c80f
Tobias K <tobias.kerzenmacher@kit.edu>
2022-02-16 15:47:46 +0100

    start of new reading routine for ancistrus MIPAS files.

---
+++ imktk/imktk/dataarray_methods/read_ancistrus.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+import pandas as pd
+import xarray as xr
+
+
+def read_var(varname,filename): 
+    
+
+def main(data):
+
+
+if __name__ == "__main__":
+    main(data)
+
+
+
+
