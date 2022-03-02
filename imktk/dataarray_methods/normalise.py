22920dcaad3799940be97fc61f6199979d7ab4b5
ucyo <cayoglu@me.com>
2022-03-02 16:15:04 +0100

    Fix static analysis via black + flake

--- imktk/dataarray_methods/normalise.py
+++ imktk/dataarray_methods/normalise.py
@@ -3,12 +3,20 @@
 """Calculate normalisation within a xr.DataArray."""
 import xarray as xr
 
+
 def standard(arr):
+    print("Using standard normalisation")
     pass
+
 
 def t_stats(arr):
+    print("Using t-stats normalisation")
     pass
 
-def main(dataarray):
+
+def main(dataarray, mode):
     assert isinstance(dataarray, xr.DataArray), "Input is not an xr.DataArray"
-    return standard(dataarray)
+    if mode == "standard":
+        return standard(dataarray)
+    else:
+        return t_stats(dataarray)
