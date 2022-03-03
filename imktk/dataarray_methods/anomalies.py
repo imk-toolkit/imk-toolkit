40e7056981911db746cf2b3a31466ebe45930e9f
Tobias K <tobias.kerzenmacher@kit.edu>
2022-03-03 16:12:41 +0100

    add method climatology

--- imktk/dataarray_methods/anomalies.py
+++ imktk/dataarray_methods/anomalies.py
@@ -2,11 +2,11 @@
 # coding: utf-8
 """Calculate anomalies within a xr.DataArray."""
 import xarray as xr
+from imktk.dataarray_methods import climatology
 
 
 def main(dataarray):
     """Return monthly anomalies for dataarray."""
     assert isinstance(dataarray, xr.DataArray), "Input is not an xr.DataArray"
-    climatology = dataarray.groupby("time.month").mean("time")
-    anomalies = dataarray.groupby("time.month") - climatology
+    anomalies = dataarray.groupby("time.month") - climatology(dataarray)
     return anomalies
---
+++ imktk/dataarray_methods/climatology.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""Calculate climatology within a xr.DataArray."""
+import xarray as xr
+
+
+def main(dataarray):
+    """Return monthly climatology for dataarray."""
+    assert isinstance(dataarray, xr.DataArray), "Input is not an xr.DataArray"
+    return dataarray.groupby("time.month").mean("time") 
