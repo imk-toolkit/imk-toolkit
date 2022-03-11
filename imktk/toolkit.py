335071b9e2983c5e5f9dec46d7aeb6a71dc4e5e8
ucyo <cayoglu@me.com>
2022-03-11 10:58:42 +0100

    Proper logging setup with command line setting. Fixes #70
    Change default logging and add it to environment file

--- imktk/toolkit.py
+++ imktk/toolkit.py
@@ -10,8 +10,22 @@ import logging
 
 _DA_ENV = "XSUITE_DA_METHODS"
 _DS_ENV = "XSUITE_DS_METHODS"
+_LOG_LEVEL = "IMKTK_LOGLEVEL"
 
+def _get_log_level_via_env():
+    loglevel = os.environ.get(_LOG_LEVEL, "ERROR")
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f'Invalid log level: {loglevel}')
+    return numeric_level
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=_get_log_level_via_env()
+    )
 logger = logging.getLogger(__name__)
+
 
 
 @xr.register_dataarray_accessor("imktk")
@@ -80,12 +94,14 @@ def _add_folder(folder, mode=None):
 
     pythonfiles = [x[:-3] for x in os.listdir(folder) if x.endswith(".py") and x[0] != "_"]
     if not pythonfiles:
-        return False
+        logger.warning("Folder %s is empty. No methods added.", folder)
+        return None
     sys.path.insert(0, folder)
     for method in pythonfiles:
         try:
             lib = ilib.import_module(method, package="imktk")
             _patch(getattr(lib, "main"), method, mode)
+            logger.info("Method: %s added in mode: %s", method, mode)
         except (SystemError, ImportError) as err:
             logger.error("Method: %s not loaded. Because: %s", method, err)
         except AttributeError as err:
@@ -100,6 +116,7 @@ def _load_from_env(mode=None):
     envvar = _DA_ENV if mode == "da" else _DS_ENV
     env = os.environ.get(envvar, False)
     if not env:
+        logger.warning("ENV %s not set. No methods added.", env)
         return None
     folders = env.split(":")
     for folder in folders:
