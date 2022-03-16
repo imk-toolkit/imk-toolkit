94181eab27536b9364a1e078b4c51ecba3b7b6cb
ucyo <cayoglu@me.com>
2022-03-16 12:28:45 +0100

    Add template files for scripts

---
+++ imktk/dataset_methods/_template.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Multi-line documentation placeholder if whole module needs to be explained.
+
+Multi line documentation for data processing. Multi line documentation
+for data processing. Multi line documentation for data processing.
+Multi line documentation for data processing. Multi line documentation
+for data processing. Multi line documentation for data processing.
+Multi line documentation for data processing.
+"""
+import xarray as xr
+
+
+def main(dataset):
+    """One line documentation for data processing.
+
+    Multi line documentation for data processing. Multi line documentation
+    for data processing. Multi line documentation for data processing. Multi
+    line documentation for data processing. Multi line documentation for
+    data processing. Multi line documentation for data processing. Multi
+    line documentation for data processing. Multi line documentation for
+    data processing.
+
+    Arguments
+    =========
+    dataset: <Description>
+    argument1: <Description>
+    ...
+    argumentX: <Description>
+
+    Returns
+    =======
+    dataset: <Description>
+    """
+    assert isinstance(dataset, xr.Dataset), "Input is not a xr.Dataset"
+
+    # add data processing code here
+
+    return dataset
