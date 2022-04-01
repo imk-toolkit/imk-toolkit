04557f6b6ad54f0c50d0a36e651403fdb3126a17
nicole.buettner <gi2423@kit.edu>
2022-04-01 07:34:23 +0200

    vapour_pres_liq.py initial commit

---
+++ imktk/dataarray_methods/vapour_pres_liq.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+Calculate saturation vapour pressure over liquid water for temperatures 123 K < T < 332 K according to
+Murphy & Koop.
+"""
+import xarray as xr
+import numpy as np
+
+
+def main(T):
+    """Return saturation vapour pressure over liquid water in Pa for given Temperature in K.
+
+    Arguments
+    =========
+    T: xr.DataArray
+        in K
+
+    Returns
+    =======
+    dataarray: saturation vapour pressure over liquid water in Pa.
+    """
+    assert isinstance(T, xr.DataArray), "Input is not an xr.DataArray"
+
+    p_liq = np.exp(54.842763 - 6763.22 / T - 4.21 * np.log(T) + 0.000367 * T + np.tanh(0.0415 * (T - 218.8)) \
+                   * (53.878 - 1331.22 / T - 9.44523 * np.log(T) + 0.014025 * T))
+    return p_liq
+
