1d05bd866c7d20e94b51853b457343835d1f80ee
ucyo <Ugur.Cayoglu@kit.edu>
2022-04-27 15:47:27 +0200

    Update documentation of anomalies

--- imktk/dataarray_methods/anomalies.py
+++ imktk/dataarray_methods/anomalies.py
@@ -6,7 +6,16 @@ from climatology import main as climatology
 
 
 def main(dataarray):
-    """Return monthly anomalies for dataarray."""
+    """Return monthly anomalies for dataarray.
+    
+    Arguments
+    =========
+    dataarray: Climate dataarray with `time` coordinate
+
+    Returns
+    =======
+    dataarray: Normalized climate dataarray with `time` coordinate on months resolution
+    """
     assert isinstance(dataarray, xr.DataArray), "Input is not an xr.DataArray"
     anomalies = dataarray.groupby("time.month") - climatology(dataarray)
     return anomalies
