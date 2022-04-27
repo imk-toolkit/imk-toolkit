4cd982d986ccbe6bdec0953f480f4262c659f1fa
ucyo <cayoglu@me.com>
2022-04-27 15:33:20 +0200

    Initial commit for normalization feature

---
+++ imktk/dataarray_methods/normalize.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Multi-line documentation placeholder if whole module needs to be explained.
+
+Multi line documentation for data processing. Multi line documentation
+for data processing. Multi line documentation for data processing.
+Multi line documentation for data processing. Multi line documentation
+for data processing. Multi line documentation for data processing.
+Multi line documentation for data processing.
+"""
+import xarray as xr
+
+
+def main(dataarray):
+    """One line documentation for data processing.
+
+    Multi line documentation for data processing. Multi line documentation
+    for data processing. Multi line documentation for data processing. Multi
+    line documentation for data processing. Multi line documentation for
+    data processing. Multi line documentation for data processing. Multi
+    line documentation for data processing. Multi line documentation for
+    data processing.
+
+    Arguments
+    =========
+    dataarray: <Description>
+    argument1: <Description>
+    ...
+    argumentX: <Description>
+
+    Returns
+    =======
+    dataarray: <Description>
+    """
+    assert isinstance(dataarray, xr.DataArray), "Input is not an xr.DataArray"
+
+    # add data processing code here
+
+    return dataarray
